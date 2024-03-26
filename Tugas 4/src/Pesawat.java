0821ff7e954823425556e343cd7c753b00e6f222
WahyuEnak <160575040+WahyuEnak@users.noreply.github.com>
2024-03-26 14:18:23 +0700

    Update Pesawat.java

--- Tugas 4/src/Pesawat.java
+++ Tugas 4/src/Pesawat.java
@@ -15,8 +15,19 @@ class Plane {
         this.tempat = tempat;
     }
 
-    public void bawaPenumpang () {
-        System.out.println(jenisMesin + " adalah jenis mesin yang biasa dipakai pada pesawat penumpang");
+    // Method overloading dengan baawapenumpang (1)
+    public void bawaPenumpang() {
+        System.out.println("Pesawat ini membawa penumpang.");
+    }
+
+    // Method overloading dengan baawapenumpang (2)
+    public void bawaPenumpang(int jumlahPenumpang) {
+        System.out.println("Pesawat ini membawa " + jumlahPenumpang + " penumpang.");
+    }
+
+    // Method overloading dengan baawapenumpang (3)
+    public void bawaPenumpang(String tujuan, int jumlahPenumpang) {
+        System.out.println("Pesawat menuju " + tujuan + " dengan membawa " + jumlahPenumpang + " penumpang.");
     }
 
     public void terbang () {
@@ -42,6 +53,8 @@ class pesawat  {
         Plane Pesawatcuy = new Plane("Turbojet Engine", "700-800 KM/J", "KT-1B Wongbee", "Malang");
 
         Pesawatcuy.bawaPenumpang();
+        Pesawatcuy.bawaPenumpang(45);
+        Pesawatcuy.bawaPenumpang("Surabaya",367);
         Pesawatcuy.terbang();
         Pesawatcuy.freestyle();
         Pesawatcuy.jatuh();
