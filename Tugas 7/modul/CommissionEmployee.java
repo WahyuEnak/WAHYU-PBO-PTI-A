2f2c95cc20024608eb4f807f6008d1db9e7d60c4
WahyuEnak <160575040+WahyuEnak@users.noreply.github.com>
2024-05-04 20:45:47 +0700

    Create CommissionEmployee.java

---
+++ Tugas 7/modul/CommissionEmployee.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+public class CommissionEmployee extends Employee {
+    private double grossSales;// penjualan per minggu
+    private double commissionRate;// komisi
+
+    public CommissionEmployee(String name, String noKTP, double sales, double rate, LocalDate birthDate) {
+        super(name, noKTP, birthDate);
+        setGrossSales(sales);
+        setCommissionRate(rate);
+    }
+
+    public void setGrossSales(double sales) {
+        grossSales = sales;
+    }
+
+    public double getGrossSales() {
+        return grossSales;
+    }
+
+    public void setCommissionRate(double rate) {
+        commissionRate = rate;
+    }
+
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    public double earnings() {
+        return getCommissionRate() * getGrossSales();
+    }
+
+    public String toString(){
+            return String.format("Commision employee: "+super.toString()+"\ngross sales: "+getGrossSales()+"\ncommission rate"+getCommissionRate());
+    }
+}
