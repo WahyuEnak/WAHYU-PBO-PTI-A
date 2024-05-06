8dafe911407485a28848d92acabd20bc3aa1a728
WahyuEnak <160575040+WahyuEnak@users.noreply.github.com>
2024-05-06 21:42:27 +0700

    Update Main.java

--- Tugas 7/src/Main.java
+++ Tugas 7/src/Main.java
@@ -1,35 +1,85 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        KueCoy[] kueArray = new KueCoy[20];
+        Scanner scanner = new Scanner(System.in);
+        KueCoy[] kueArray = new KueCoy[25];
 
         // Isi array dengan berbagai jenis kue
         kueArray[0] = new KuePesanan("Brownies", 20000.0, 1.5);
         kueArray[1] = new KuePesanan("Lapis Legit", 25000.0, 2.0);
-        kueArray[2] = new KuePesanan("Cheesecake", 30000.0, 1.0);
-        kueArray[3] = new KuePesanan("Red Velvet Cake", 35000.0, 1.2);
-        kueArray[4] = new KuePesanan("Tiramisu", 40000.0, 1.8);
-        kueArray[5] = new KuePesanan("Donat", 5000.0, 10);
-        kueArray[6] = new KuePesanan("Croissant", 10000.0, 6);
-        kueArray[7] = new KuePesanan("Bolu Gulung", 15000.0, 4);
-        kueArray[8] = new KuePesanan("Pancake", 12000.0, 5);
-        kueArray[9] = new KuePesanan("Kue Lumpur", 4000.0, 10);
+        kueArray[2] = new KuePesanan("Kue Lumpur", 25000.0, 2.0);
+        kueArray[3] = new KuePesanan("Kue Pie", 25000.0, 2.0);
+        kueArray[4] = new KuePesanan("Kue Pukis", 25000.0, 2.0);
+        kueArray[5] = new KuePesanan("Kue Nastar", 25000.0, 2.0);
+        kueArray[6] = new KuePesanan("Kue Putri Salju", 25000.0, 2.0);
+        kueArray[7] = new KuePesanan("Kue Tiramissu", 25000.0, 2.0);
+        kueArray[8] = new KuePesanan("Crossiant", 25000.0, 2.0);
+        kueArray[9] = new KuePesanan("Pane Cake", 25000.0, 2.0);
 
         kueArray[10] = new KueJadi("Kue Oli Samping", 11000.0, 2);
-        kueArray[11] = new KueJadi("Kue Kampas Kopling", 20000.0, 1);
-        kueArray[12] = new KueJadi("Kue Corolla Altis", 400000.0, 2);
-        kueArray[13] = new KueJadi("Kue Innova Reborn", 600000.0, 3);
-        kueArray[14] = new KueJadi("Kue Jupiter MX", 14000.0, 5);
-        kueArray[15] = new KueJadi("Kue Vario 125", 12500.0, 3);
-        kueArray[16] = new KueJadi("Kue Redmi Note 10 Pro", 3000.0, 45);
-        kueArray[17] = new KueJadi("Kue Samsung J2 Prime", 2500.0, 50);
-        kueArray[18] = new KueJadi("Kue Anomali", 0, 0);
-        kueArray[19] = new KueJadi("Kue Sigma", 1000000000.0, 1);
+        kueArray[11] = new KueJadi("Kue Karburator", 11000.0, 2);
+        kueArray[12] = new KueJadi("Kue Kampas Kopling", 11000.0, 2);
+        kueArray[13] = new KueJadi("Kue Corolla Altis", 11000.0, 2);
+        kueArray[14] = new KueJadi("Kue Innova Reborn", 11000.0, 2);
+        kueArray[15] = new KueJadi("Kue Grand Max", 11000.0, 2);
+        kueArray[16] = new KueJadi("Kue CRV", 11000.0, 2);
+        kueArray[17] = new KueJadi("Kue Jupiter MX", 11000.0, 2);
+        kueArray[18] = new KueJadi("Kue Supra Geter", 11000.0, 2);
+        kueArray[19] = new KueJadi("Kue Shoogun", 11000.0, 2);
 
-        // Tampilkan semua kue beserta jenisnya
-        System.out.println("Semua Kue:");
-        for (KueCoy kue : kueArray) {
-            System.out.println(kue.getClass().getSimpleName() + " - " + kue);
-        }
+        kueArray[20] = new KueBasi("Kue Busi Gosong", 10000.0, 5);
+        kueArray[21] = new KueBasi("Kue Oli Kering", 15000.0, 3);
+        kueArray[22] = new KueBasi("Kue Pertalite", 15000.0, 3);
+        kueArray[23] = new KueBasi("Kue Mamah Aku Takut", 15000.0, 3);
+        kueArray[24] = new KueBasi("Kue Pembohonk Publik", 15000.0, 3);
 
+        int choice;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Tampilkan Semua Kue");
+            System.out.println("2. Hitung Total Harga Kue");
+            System.out.println("3. Hitung Total Lama Hari Kue Basi");
+            System.out.println("0. Keluar");
+
+            System.out.print("Pilih menu: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Semua Kue:");
+                    for (KueCoy kue : kueArray) {
+                        if (kue != null) {
+                            System.out.println(kue.getClass().getSimpleName() + " - " + kue);
+                        }
+                    }
+                    break;
+                case 2:
+                    double totalHarga = 0;
+                    for (KueCoy kue : kueArray) {
+                        if (kue != null) {
+                            totalHarga += kue.hitungHarga();
+                        }
+                    }
+                    System.out.println("Total Harga Kue: " + totalHarga);
+                    break;
+                case 3:
+                    int totalLamaHari = 0;
+                    for (KueCoy kue : kueArray) {
+                        if (kue instanceof KueBasi) {
+                            totalLamaHari += ((KueBasi) kue).hitungHarga();
+                        }
+                    }
+                    System.out.println("Total Lama Hari Kue Basi: " + totalLamaHari);
+                    break;
+                case 0:
+                    System.out.println("Terima kasih!");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+        } while (choice != 0);
+
+        scanner.close();
     }
 }
