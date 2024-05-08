6e5f41e4e0bd871a7eca158a5e692a93c7dffb92
WahyuEnak <160575040+WahyuEnak@users.noreply.github.com>
2024-05-08 14:10:31 +0700

    Create Main.java

---
+++ Tugas 8/modul/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        // casting
+        // double angka = 5.4;
+        // int angka_int = (int)angka;
+        // System.out.println(angka_int);
+
+        // Object dengan class HeroIntel
+        HeroIntel hero1 = new HeroIntel("Ucup", 100);
+        hero1.display();
+        hero1.type = "icikiwir";
+
+        // upcasting
+        Hero heroUp = (Hero) hero1;
+        heroUp.display();
+        // System.out.println(heroUp.getType()); //ini error
+
+        // Object dgn class Hero
+        Hero heroReg = new Hero("Boy", 100);
+        heroReg.display();
+
+        // downcasting
+        // HeroAgility heroDown = (HeroAgility) heroReg; //ini error
+        // heroDown.display();
+
+        // heroUp dikembalikan ke hero1
+        HeroIntel hero2 = (HeroIntel) heroUp;
+        hero2.display(); // ini berhasil downcasting
+        hero2.type = "Aselole";
+
+        // Upcasting
+        HeroMagic heroMagic = new HeroMagic("Merlin", 150);
+        Hero heroUpMagic = (Hero) heroMagic;
+
+        // Downcasting
+        HeroMagic heroMagicDown = (HeroMagic) heroUpMagic;
+        
+        //Menguji akses atribut dan method dari objek yang telah di-downcast.
+        System.out.println("Name: " + heroMagicDown.getName());
+        System.out.println("Health: " + heroMagicDown.getHealth());
+        System.out.println("Power: " + heroMagicDown.getPower());
+        heroMagicDown.display();
+
+    }
+}
