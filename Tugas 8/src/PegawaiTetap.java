0a6559395efc13b1eb206162af6e82af20e34ea4
WahyuEnak <160575040+WahyuEnak@users.noreply.github.com>
2024-05-08 14:06:24 +0700

    Create PegawaiTetap.java

---
+++ Tugas 8/src/PegawaiTetap.java
@@ -0,0 +1,32 @@
+class PegawaiTetap extends Pegawai {
+    // Atribut
+    private double upah;
+
+    // Konstruktor
+    public PegawaiTetap(String nama, String noKTP, double upah) {
+        super(nama, noKTP);
+        this.upah = upah;
+    }
+
+    // Implementasi method hitungGaji() untuk Pegawai Tetap
+    @Override
+    public double hitungGaji() {
+        return upah;
+    }
+}
+class PegawaiTetap extends Pegawai {
+    // Atribut
+    private double upah;
+
+    // Konstruktor
+    public PegawaiTetap(String nama, String noKTP, double upah) {
+        super(nama, noKTP);
+        this.upah = upah;
+    }
+
+    // Implementasi method hitungGaji() untuk Pegawai Tetap
+    @Override
+    public double hitungGaji() {
+        return upah;
+    }
+}
